337f47cf1e764eeb05e7419012c802c78c98f454
Harald Kohlmann <Harald.Kohlmann@gmx.de>
2023-12-04 15:23:22 +0100

    reactivate pytest

---
+++ PYnativePythonProgramming/python-input-and-output-exercise/test-Exercise6.py
@@ -0,0 +1,13 @@
+# Write all content of a given file into a new file by skipping line number 5
+
+with open('new_file.txt', 'w') as new_file, open('test.txt', 'r') as base_file:
+    line_number = 0
+
+    for line in base_file:
+        line_number+=1
+        
+        if line_number != 5:
+            new_file.write(line)
+
+new_file = open('new_file.txt', 'r')
+print(new_file.read())
